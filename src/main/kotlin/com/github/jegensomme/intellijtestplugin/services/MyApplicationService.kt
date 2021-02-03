ca21c7097102a5459d4a25e56bb3e6be4e6d4684
GitHub Action <action@github.com>
2021-02-03 12:43:30 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jegensomme/intellijtestplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jegensomme.intellijtestplugin.services
+
+import com.github.jegensomme.intellijtestplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/jegensomme/intellijtestplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jegensomme/intellijtestplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jegensomme.intellijtestplugin.services
 
+import com.github.jegensomme.intellijtestplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
